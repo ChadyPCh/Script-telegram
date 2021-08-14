f4ec9ab7789bb121e1d158a416b16b3fc5de6215
ChadyPCh <88913382+ChadyPCh@users.noreply.github.com>
2021-08-14 16:55:13 -0400

    Update All.py

--- All.py
+++ All.py
@@ -1,19 +1,14 @@
 import os
 import threading
 
-def PedroSam():
-  os.system("python3 PedroSam.py")
+def Dayi():
+  os.system("python3 Dayi.py")
 
-def Bia():
-  os.system("python3 Bia.py")
+def Alcapone():
+  os.system("python3 Alcapone.py")
 
-def Richar():
-  os.system("python3 Richar.py")
-
-t1= threading.Thread(target=PedroSam)
-t2= threading.Thread(target=Bia)
-t3= threading.Thread(target=Richar)
+t1= threading.Thread(target=Dayi)
+t2= threading.Thread(target=Alcapone)
 
 t1.start()
 t2.start()
-t3.start()
