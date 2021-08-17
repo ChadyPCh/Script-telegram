e19dbc35512ad9b33e89dc8593896943117f99b9
ChadyPCh <88913382+ChadyPCh@users.noreply.github.com>
2021-08-16 22:24:21 -0400

    Update All.py

--- All.py
+++ All.py
@@ -7,8 +7,13 @@ def Dayi():
 def Alcapone():
   os.system("python3 Alcapone.py")
 
+def Yenna():
+  os.system("python3 Yenna.py")
+
 t1= threading.Thread(target=Dayi)
 t2= threading.Thread(target=Alcapone)
+t3= threading.Thread(target=Yenna)
 
 t1.start()
 t2.start()
+t3.start()
