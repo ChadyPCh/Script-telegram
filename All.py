2c11cb6ceb2717537c6104c5a32e0894c62eedea
ChadyPCh <88913382+ChadyPCh@users.noreply.github.com>
2021-08-22 19:16:51 -0400

    Update All.py

--- All.py
+++ All.py
@@ -13,12 +13,22 @@ def Yenna():
 def Brenda():
   os.system("python3 Brenda.py")
 
+def Duwars():
+  os.system("python3 Duwars.py")
+
+def Resurrection():
+  os.system("python3 altChacon2.py")
+
 t1= threading.Thread(target=Dayi)
 t2= threading.Thread(target=Alcapone)
 t3= threading.Thread(target=Yenna)
 t4= threading.Thread(target=Brenda)
+t5= threading.Thread(target=Duwars)
+t6= threading.Thread(target=Resurrection)
 
 t1.start()
 t2.start()
 t3.start()
 t4.start()
+t5.start()
+t6.start()
