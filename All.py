7f244347eba9e8bdc391500ed6f933039dba4a00
ChadyPCh <88913382+ChadyPCh@users.noreply.github.com>
2021-08-23 00:04:26 -0400

    Update All.py

--- All.py
+++ All.py
@@ -22,6 +22,9 @@ def Resurrection():
 def Noelle():
   os.system("python3 Noelle.py")
 
+def Baam():
+  os.system("python3 Baam.py")
+
 t1= threading.Thread(target=Dayi)
 t2= threading.Thread(target=Alcapone)
 t3= threading.Thread(target=Yenna)
@@ -29,6 +32,7 @@ t4= threading.Thread(target=Brenda)
 t5= threading.Thread(target=Duwars)
 t6= threading.Thread(target=Resurrection)
 t7= threading.Thread(target=Noelle)
+t8= threading.Thread(target=Baam)
 
 t1.start()
 t2.start()
@@ -37,3 +41,4 @@ t4.start()
 t5.start()
 t6.start()
 t7.start()
+t8.start()
