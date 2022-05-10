c696e41dc823d09aa75839726f39cd7b63c13706
ChadyPCh <88913382+ChadyPCh@users.noreply.github.com>
2022-05-09 22:41:30 -0400

    Update Brenda.py

--- Brenda.py
+++ Brenda.py
@@ -39,9 +39,11 @@ async def new_quest_handle(event):
         await event.click(0)     
 
     if gw==1:
-        if "Withdrawing" in event.raw_text:
+        if "Withdrawing" in event.raw_text or "Not enough items on guild stock" in event.raw_text or "You are too busy with a different adventure. Try a bit later." in event.raw_text:
             await client.forward_messages(-1001362175569,event.message) 
             gw=0 
+        else:
+            gw=0
 
     if "To accept their offer, you shall" in event.raw_text:
         time.sleep(randint(1,5))
